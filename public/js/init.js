2b6f53538ae6f7d0df5b0d8d26b9f91b10fe8787
Jimmy Skinnari <jimmyskinnari@hotmail.com>
2021-04-09 00:17:20 +0200

    Update init.js

--- public/js/init.js
+++ public/js/init.js
@@ -1,58 +1,41 @@
 
 window.onload = () => {
-
     
     localStorage.removeItem("LoggedInUser");
 
-
     $("#login").click( () => {
-
         let username = $("#username").val();
         let password = $("#password").val();
-
         let user = new User(username, password);
-
         user.Login();
     });
 
-
     $("#sign-up").click( () => {
-
-
         $('#recipe-new-modal').modal('hide');
         $('#sign-up-new-modal').modal('show');
-
     });
 
 
     $("#register").click( () => {
-
         let username = $("#username-new").val();
         let password = $("#password-new").val();
-
         let user = new User(username, password);
-
         user.AddUser();
-
     });
 
     
 
     function validatePassword(password1, password2){
-
         flag = false;
         if(password1 == password2){
     
             flag = true;
-        }
-    
+        }    
         return flag;
     }
 
-
     // Toggle element hide & show
-    function Toggle(x) {
-        
+    function Toggle(x) {       
         if (x.style.display === "none") {
           x.style.display = "block";
         } else {
